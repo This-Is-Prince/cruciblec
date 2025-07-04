f48c7ab43c59df76f276b4151df2964f920e7eec
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-07-04 12:31:07 +0530

    feat: arithmetic

---
+++ arithmetic/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // arithmetic operators = + - * / % ++ --
+
+    int x = 2;
+    int y = 3;
+    int z = 0;
+
+    z = x + y; // Addition
+    printf("Addition: %d + %d = %d\n", x, y, z);
+
+    return 0;
+}
